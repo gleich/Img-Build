e7b7adc184e50fc50fdae1dd6321d3845f2f4198
Matthew Gleich <matthewgleich@gmail.com>
2019-10-17 19:46:20 -0400

    Use new file_utils reading function

--- src/github_interactions.py
+++ src/github_interactions.py
@@ -1,23 +1,17 @@
 from github import Github
-import yaml
+import file_utils
+
 
 def authenticate():
-    opened_file = False
-    try:
-        with open("config.yml", "r") as config_file:
-            data = yaml.safe_load(config_file)
-        opened_file = True
-    except FileNotFoundError():
-        raise Warning("Please make sure that the config.yml file is in the src directory")
-    token_warning = "There is no personalAccessToken in the config.yml file. It is needed to authenticated"
-    try:
-        if data[1]["personalAccessToken"] == "":
-            raise Warning(token_warning)
-    except KeyError():
-        raise Warning(token_warning)
-    account_object = Github(data[1]["personalAccessToken"])
-    return account_object
-        
+    """Authenticate the user for the Github API
     
+    Returns:
+        object -- object for the account
+    """
+    config = file_utils.safe_file_read("GHpersonalAccessToken.txt", "txt")
+    account_object = Github(config)
+    return account_object
+
+
 # Testing:
 # authenticate()
