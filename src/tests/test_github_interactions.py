53b5db1fb8f7ea152b24a17abf1161143976f216
Matthew Gleich <matthewgleich@gmail.com>
2019-10-18 11:02:09 -0400

    Create test for get_repo_info function

--- src/tests/test_github_interactions.py
+++ src/tests/test_github_interactions.py
@@ -1,5 +1,16 @@
 import github_interactions
 import os
+import json
+
+
+def test_get_repo_info():
+    """Test the get_repo_info functioN in the github_interactions.py file
+    """
+    impractical_python_projects_response = github_interactions.get_repo_info(
+        "Matt-Gleich/Impractical_Python_Projects")
+    with open("repo_info_response.json", "r") as response_file:
+        expected_response = json.load(response_file)
+    assert impractical_python_projects_response == expected_response
 
 
 def test_clone_repo():
