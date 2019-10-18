d2933ab6051f2e093b10fa9ebd2dd136e852871e
Matthew Gleich <matthewgleich@gmail.com>
2019-10-17 20:22:53 -0400

    Use current dir context for file loading

--- src/tests/test_file_utils.py
+++ src/tests/test_file_utils.py
@@ -2,14 +2,14 @@ import file_utils
 
 def test_safe_file_read():
     # yml tests:
-    yml_reading = file_utils.safe_file_read("./tests/read_test.yml", "yml")
+    yml_reading = file_utils.safe_file_read("read_test.yml", "yml")
     assert yml_reading == [{"nameOfUser": "Matt-Gleich"}]
 
     # json tests:
-    json_reading = file_utils.safe_file_read("./tests/read_test.json", "json")
+    json_reading = file_utils.safe_file_read("read_test.json", "json")
     assert json_reading == {'nameOfUser': 'Matt-Gleich'}
 
     # txt tests:
-    txt_reading = file_utils.safe_file_read("./tests/read_test.txt", "txt")
+    txt_reading = file_utils.safe_file_read("read_test.txt", "txt")
     assert txt_reading == "Matt-Gleich"
     
