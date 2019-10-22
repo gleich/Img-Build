82246b60fb64ded9455d72c15dc01cdf2b9f0538
Matthew Gleich <matthewgleich@gmail.com>
2019-10-22 10:55:21 -0400

    Update function call for new file name

--- src/tests/test_git_interactions.py
+++ src/tests/test_git_interactions.py
@@ -5,10 +5,10 @@ import os
 def test_clone_repo():
     """Test the clone_repo function in the github_interactions.py
     """
-    repo1 = github_interactions.clone_repo(
+    repo1 = git_interactions.clone_repo(
         "https://github.com/goffstown-sports-app/Scrape-Calendar-Data.git")
     assert repo1 == ["Scrape-Calendar-Data"]
-    repo2 = github_interactions.clone_repo(
+    repo2 = git_interactions.clone_repo(
         "https://github.com/goffstown-sports-app/Config-Files.git")
     assert repo2 == ["Scrape-Calendar-Data",
                      "Config-Files"] or repo2 == ["Config-Files", "Scrape-Calendar-Data"]
