84d0ccd42bdcf8e67169b1a2f357006b29788e31
Matthew Gleich <matthewgleich@gmail.com>
2019-10-22 10:52:52 -0400

    Rename to git_interactions

--- src/git_interactions.py
+++ src/git_interactions.py
@@ -5,22 +5,6 @@ import requests
 
 import file_utils
 
-def get_repo_info(full_repo_name):
-    url = "https://api.github.com/repos/" + full_repo_name
-    headers = {
-        'Accept': "*/*",
-        'Cache-Control': "no-cache",
-        'Host': "api.github.com",
-        'Accept-Encoding': "gzip, deflate",
-        'Connection': "keep-alive",
-        'cache-control': "no-cache"
-    }
-    response = requests.request("GET", url, headers=headers)
-    return response.json()
-    
-# Testing:
-# get_repo_info("goffstown-sports-app/Scrape-Calendar-Data")
-
 
 def clone_repo(cloneURL):
     """Clone a repo into the repos folder
