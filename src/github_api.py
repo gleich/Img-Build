c3d22f937d98b4049ce42c5f5111649b44597dd1
Matthew Gleich <matthewgleich@gmail.com>
2019-10-22 21:02:56 -0400

    Add docs

--- src/github_api.py
+++ src/github_api.py
@@ -2,6 +2,14 @@ import requests
 
 
 def get_repo_info(full_repo_name):
+    """Get the meta data for the repo
+    
+    Arguments:
+        full_repo_name {string} -- the username/repository_name of the repo
+    
+    Returns:
+        dict -- response from 
+    """
     url = "https://api.github.com/repos/" + full_repo_name
     headers = {
         'Accept': "*/*",
