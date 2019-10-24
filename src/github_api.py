b6b848fc4f8fc909005eae9e8c267bd42fb7db96
Matthew Gleich <matthewgleich@gmail.com>
2019-10-24 10:15:08 -0400

    Create get_repo_commit function

--- src/github_api.py
+++ src/github_api.py
@@ -3,10 +3,10 @@ import requests
 
 def get_repo_info(full_repo_name):
     """Get the meta data for the repo
-    
+
     Arguments:
         full_repo_name {string} -- the username/repository_name of the repo
-    
+
     Returns:
         dict -- response from 
     """
@@ -24,3 +24,37 @@ def get_repo_info(full_repo_name):
 
 # Testing:
 # get_repo_info("goffstown-sports-app/Scrape-Calendar-Data")
+
+
+def get_repo_commits(full_repo_name):
+    url = "https://api.github.com/repos/" + full_repo_name + "/stats/contributors"
+    headers = {
+        'Accept': "*/*",
+        'Cache-Control': "no-cache",
+        'Host': "api.github.com",
+        'Accept-Encoding': "gzip, deflate",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+    }
+    orig_response = requests.request(
+        "GET", url, headers=headers).json()
+    total_commit_amount = 0
+    for contributor in orig_response:
+        total_commit_amount += contributor["total"]
+    if total_commit_amount == 0:
+        requests_amount = 0
+        while requests_amount < 10:
+            new_response = requests.request(
+                "GET", url, headers=headers).json()
+            requests_amount += 1
+            for contributor in new_response:
+                total_commit_amount += contributor["total"]
+            if total_commit_amount != 0:
+                break
+            else:
+                continue
+    return total_commit_amount
+
+
+# Testing:
+# print(get_repo_commits("Team-501-The-PowerKnights/Vision2019"))
--- src/tests/test_github_api.py
+++ src/tests/test_github_api.py
@@ -10,3 +10,10 @@ def test_get_repo_info():
     with open("repo_info_response.json", "r") as response_file:
         expected_response = json.load(response_file)
     assert impractical_python_projects_response == expected_response
+    
+def test_get_repo_commits():
+    """Test for the get_repo_commits function in the github_api.py
+    """
+    impractical_python_projects_response = github_api.get_repo_commits(
+        "Matt-Gleich/Impractical_Python_Projects")
+    assert impractical_python_projects_response == 5
