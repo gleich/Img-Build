729b55e423fbd403d29cbe0357dd18451f7635e5
Matthew Gleich <matthewgleich@gmail.com>
2019-10-25 23:08:01 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from subprocess import call
 import os
-import termcolor
 
+from printing_utils import print_with_time
 import file_utils
 import git_interactions
 import github_api
@@ -13,20 +13,35 @@ def main():
     repos = []
     for repo in configuration_file["repos"]:
         git_interactions.clone_repo(repo["cloneURL"])
+        print_with_time("📂 Gathering number of commits for " + repo, "yellow")
+        print_with_time(
+            "✅ Successfully gathered number of commits for " + repo, "green")
         repo_commit_amount = github_api.get_repo_commits(repo)
         repo_commit_nums[repo] = repo_commit_amount
     docker_username = configuration_file["docker"]["userName"]
     docker_password = configuration_file["docker"]["password"]
-    # echo ${{ secrets.docker_password }} | docker login -u ${{ secrets.docker_id }} --password-stdin
-    print(termcolor.colored("🔑 Logining into Docker Hub", "yellow"))
+    print_with_time("🔑  Logining into Docker Hub", "yellow")
     call(["echo", "'" + docker_password + "'", "|", "docker",
           "login", "-u", docker_username, "--password-stdin"])
-    print(termcolor.colored("✅ Logged into Docker Hub", "yellow"))
+    print_with_time("✅ Successfully logged into Docker Hub", "yellow")
     while True:
         for repo in repos:
             newest_commit_amount = github_api.get_repo_commits(repos[repo])
             if newest_commit_amount > repos[repo]:
+                print_with_time("🚀 " + repo + " updated; " + str(repos[repo]) + " commits --> " + str(newest_commit_amount) + " commits", "blue")
+                repos[repo] = newest_commit_amount
                 os.chdir("./repos/" + repo)
-                call(["docker", "build", "-t", docker_username])
+                imageName = configuration_file["repos"][repo]["imageName"]
+                imageTag = configuration_file["repos"][repo]["imageTag"]
+                print_with_time("🐳 Building image for " + docker_username + "/" + imageName + ":" + imageTag, "yellow")
+                call(["docker", "build", "-t", docker_username + "/" + imageName + ":" + imageTag])
+                print_with_time("🐳 Successfully built image for " + docker_username +
+                                "/" + imageName + ":" + imageTag, "yellow")
+                os.chdir("../..")
+            else:
+                print_with_time("💤 Nothing has changed for " + repo, "blue")
+        print_with_time("🏁 Finshed Cycle")
+        print_with_time("⏰ Waiting 10 seconds for next repo check")
+            
     
 main()
