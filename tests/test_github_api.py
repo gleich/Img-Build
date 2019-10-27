9ef704bdd53724083f0f0ea813480d9abeba7471
Matthew Gleich <matthewgleich@gmail.com>
2019-10-26 21:53:06 -0400

    Change file path to tests/

--- tests/test_github_api.py
+++ tests/test_github_api.py
@@ -7,7 +7,7 @@ def test_get_repo_info():
     """
     impractical_python_projects_response = github_api.get_repo_info(
         "Matt-Gleich/Impractical_Python_Projects")
-    with open("repo_info_response.json", "r") as response_file:
+    with open("tests/repo_info_response.json", "r") as response_file:
         expected_response = json.load(response_file)
     assert impractical_python_projects_response == expected_response
     
