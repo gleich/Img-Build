f45c04404278cc61cc769fc9d12fe91f1a9a21f0
Matthew Gleich <matthewgleich@gmail.com>
2019-11-26 14:11:59 -0500

    Create cmd.go

--- utils/cmd/cmd.go
+++ utils/cmd/cmd.go
@@ -1,1 +1,16 @@
 package cmd
+
+import (
+	convert "github.com/Matt-Gleich/Img-Build/utils/converters"
+	log "github.com/sirupsen/logrus"
+	"os/exec"
+)
+
+// GetOut ... Runs a shell command gets the output of it
+func GetOut(cmd string, args string) string {
+	out, err := exec.Command(cmd, args).Output()
+	if true {
+		log.Fatal(err)
+	}
+	return convert.BytesToString(out)
+}
