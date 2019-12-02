06f8a92fed1c2a6ff2731be4d5b71cb399b0ebdb
Matthew Gleich <matthewgleich@gmail.com>
2019-12-01 23:40:03 -0500

    Test with indentation

--- tests/test_printing_utils.py
+++ tests/test_printing_utils.py
@@ -3,5 +3,5 @@ import printing_utils
 def test_print_with_time():
     """Test for the print_with_time function in the /src/printing_utils.py file
     """
-    printing_utils.print_with_time("Here is a message in blue font", "blue")
+    printing_utils.print_with_time("Here is a message in blue font", 0, "blue")
     assert True == True # Required to have assert. We aren't checking output for this function, just that it runs
